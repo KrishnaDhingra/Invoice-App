7d4d8857e50a5ce1ed8b51df52456119dafc2050
Krishna Dhingra <dkrishna1608@gmail.com>
2021-09-28 08:56:08 +0530

    Making the data.js file

---
+++ src/components/data.js
@@ -0,0 +1,30 @@
+export const MainData = [
+    {
+        id: '#XM9141',
+        status: 'draft',
+        total: '$268',
+
+        billFrom:{
+            streeetAddress: '',
+            city: '',
+            postCode: '',
+            country: ''
+        },
+        billTo:{
+            clientName: 'Krishna Dhingra',
+            clientEmail: '',
+            streeetAddress: '',
+            city: '',
+            postCode: '',
+            country: '',
+            invoiceDate: 'Due 20-09-2021',
+            paymentTerms: '',
+            description: ''
+       },
+       items:{
+           itemName: '',
+           quantity: '',
+           price: ''
+       }
+    }
+]
